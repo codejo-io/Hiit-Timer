f93546a2faa065d55b7ebc78b3f026a19d2b684f
Matt Casanova <lazersquad@gmail.com>
2020-05-25 14:50:35 -0700

    First Commit for hiit-timer

---
+++ Hiit Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hiit Timer
+//
+//  Created by Matt Casanova on 5/25/20.
+//  Copyright © 2020 Codejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
